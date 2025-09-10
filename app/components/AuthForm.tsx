0f0538f1ddb4d66b752bff01e536b834e3b7524f
Srinand Diddi <srinand.com@gmail.com>
2025-09-10 22:46:18 +0530

    clean up

--- app/components/AuthForm.tsx
+++ app/components/AuthForm.tsx
@@ -1,17 +1,13 @@
 "use client";
 import { auth } from "@/lib/firebase/client";
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-} from "firebase/auth";
-import type { FormEvent } from "react";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { type FormEvent } from "react";
 import { useRouter } from "next/navigation";
+// import { toast } from "react-toastify";
+import { FirebaseError } from "firebase/app";
 
-interface AuthForm {
-  AuthType: "Login" | "Signup";
-}
-
-export const AuthForm = ({ AuthType }: AuthForm) => {
+// TODO: add loading state, error handling, and form validation
+export const AuthForm = ({ AuthType }: { AuthType: "Login" | "Signup" }) => {
   const router = useRouter();
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
@@ -21,59 +17,58 @@ export const AuthForm = ({ AuthType }: AuthForm) => {
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
 
-    // ugly code that works
     try {
-      const user = await (AuthType === "Login"
-        ? signInWithEmailAndPassword(auth, email, password)
-        : createUserWithEmailAndPassword(auth, email, password));
-      if (AuthType === "Signup") {
-        const res = await fetch("http://localhost:4000/create-user", {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${user.user.uid}`,
-          },
-        });
-        console.log(res)
-      }
+      const userCredential = AuthType === 'Login'
+        ? await signInWithEmailAndPassword(auth, email, password)
+        : await createUserWithEmailAndPassword(auth, email, password);
+      const idToken = await userCredential.user.getIdToken();
+      console.log(idToken);
 
-      const response = await fetch("/api/login", {
-        method: "POST",
-        body: JSON.stringify({ uid: user.user.uid }),
-        headers: {
-          "Content-Type": "application/json",
-        },
+      const res = await fetch(`http://localhost:3001/${AuthType.toLowerCase()}`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ idToken }),
+        credentials: 'include',
       });
 
-      if (response.ok) router.push("/home");
+      if ((await res.json()).status === "success") {
+        // toast.success(`${AuthType} successful!`);
+        router.push("/home");
+      }
     } catch (error) {
-      console.log(error);
+      // toast.error(((error as FirebaseError).code).replace('auth/', '').replaceAll('-', ' '));
+      console.log('error: ', error);
     }
-  };
-
+  }
+  
   // TODO: make styling responsive & use variables
+  // or nvm just probably good enough for most screens :D
   return (
     <>
       <div className="h-screen w-full flex items-center justify-center">
         <form
           onSubmit={handleSubmit}
-          className="h-[320px] w-[240px] p-[10px] flex flex-col items-center justify-center gap-[10px] text-center"
+          className="h-[320px] w-[240px] p-[10px] justify-center flex flex-col gap-[10px]"
         >
-          <h1 className="text-[24px] font-bold p-[5px] w-full">{AuthType}</h1>
+          <div className="flex justify-center">
+            <h3 className="font-bold text-center">{AuthType.toLowerCase()}</h3>
+          </div>
           <div className="flex flex-col gap-[10px] items-center jusitfy-center w-full">
             <input
-              className="p-[10px] bg-[#e5e5ea] focus: border-none rounded-[4px] w-full"
+              className="form-input"
               placeholder="email"
               type="email"
               name="email"
             />
             <input
-              className="p-[10px] bg-[#e5e5ea] rounded-[4px] w-full"
+              className="form-input"
               placeholder="password"
               type="password"
               name="password"
             />
           </div>
-          <button type="submit" className="bg-[#242628] w-full p-[10px] rounded-[4px] text-white">
+          {/* bg-[#242628] w-full p-[10px] rounded-[4px] text-white cursor-pointer */}
+          <button type="submit" className="button bg-white text-black hover:invert-10 w-full">
             {AuthType}
           </button>
           <p> or {" "}
--- app/components/Icon.tsx
+++
@@ -1,7 +0,0 @@
-import Image from 'next/image'
-
-const Icon = ({name, size} : {name : string, size : number}) => {
-  return <Image className="" alt={name} src={`${name}.svg`} height={size} width={size} />
-}
-
-export default Icon
